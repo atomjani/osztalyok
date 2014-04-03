5fd4b51b74c7a11cee1b5a9f8e27fd62ef2d2da6
atomjani <atomjani@users.noreply.github.com>
2014-04-03 17:39:27 +0200

    Delete headervendeg.php

--- kinezet/headervendeg.php
+++
@@ -1,7 +0,0 @@
-<?php	
-echo '<form action="login.php" method="POST">
-Felhasználónév/email cím: <input type="text" name="felhasznalonev"/>
-Jelszó<input type="password" name="jelszo"/>  <input type="submit" name="submit" value="Belépés"/><br/>
-</form>'
-
-?>
