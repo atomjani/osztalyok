b2ad55eb676cba2ecf38ddae9163a24de50e7c5f
atomjani <atomjani@users.noreply.github.com>
2014-04-03 17:36:04 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo 'Visszajelzés(Még nincs) | Sajtóknak(Még nincs) | Felajánlások(Még nincs) | Megbízási lehetőség(Még nincs) | Pontszerzés(Hamarosan)';
-$_SESSION["url"] = "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
-
-?>
-
-</body>
-</html>
